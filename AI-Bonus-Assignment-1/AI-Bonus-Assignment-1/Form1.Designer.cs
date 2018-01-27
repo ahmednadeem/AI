2dba9231ba2433b61112232e9b7ee084bf101194
ahmednadeem <muhammadahmed644@gmail.com>
2018-01-27 21:31:30 +0500

    Bonus-Assingment
    Bonus-Assignment-1

---
+++ AI-Bonus-Assignment-1/AI-Bonus-Assignment-1/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace AI_Bonus_Assignment_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picDino = new System.Windows.Forms.PictureBox();
+            this.road = new System.Windows.Forms.PictureBox();
+            this.crow2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.crow3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timerScore = new System.Windows.Forms.Timer(this.components);
+            this.crow1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picDino)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crow2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crow3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crow1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picDino
+            // 
+            this.picDino.Image = ((System.Drawing.Image)(resources.GetObject("picDino.Image")));
+            this.picDino.Location = new System.Drawing.Point(41, 191);
+            this.picDino.Name = "picDino";
+            this.picDino.Size = new System.Drawing.Size(96, 50);
+            this.picDino.TabIndex = 0;
+            this.picDino.TabStop = false;
+            // 
+            // road
+            // 
+            this.road.Image = ((System.Drawing.Image)(resources.GetObject("road.Image")));
+            this.road.Location = new System.Drawing.Point(12, 217);
+            this.road.Name = "road";
+            this.road.Size = new System.Drawing.Size(984, 50);
+            this.road.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.road.TabIndex = 1;
+            this.road.TabStop = false;
+            // 
+            // crow2
+            // 
+            this.crow2.Image = ((System.Drawing.Image)(resources.GetObject("crow2.Image")));
+            this.crow2.Location = new System.Drawing.Point(395, 12);
+            this.crow2.Name = "crow2";
+            this.crow2.Size = new System.Drawing.Size(100, 50);
+            this.crow2.TabIndex = 3;
+            this.crow2.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(40, 21);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(209, -3);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
+            this.pictureBox9.Location = new System.Drawing.Point(473, 77);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox9.TabIndex = 9;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(571, 12);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox7.TabIndex = 10;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(625, 77);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox8.TabIndex = 11;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox10.Image")));
+            this.pictureBox10.Location = new System.Drawing.Point(254, 185);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(30, 50);
+            this.pictureBox10.TabIndex = 12;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox11.Image")));
+            this.pictureBox11.Location = new System.Drawing.Point(501, 200);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(56, 41);
+            this.pictureBox11.TabIndex = 13;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox12.Image")));
+            this.pictureBox12.Location = new System.Drawing.Point(836, 191);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(76, 50);
+            this.pictureBox12.TabIndex = 14;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Click += new System.EventHandler(this.pictureBox12_Click);
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox13.Image")));
+            this.pictureBox13.Location = new System.Drawing.Point(864, 36);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox13.TabIndex = 15;
+            this.pictureBox13.TabStop = false;
+            // 
+            // crow3
+            // 
+            this.crow3.Image = ((System.Drawing.Image)(resources.GetObject("crow3.Image")));
+            this.crow3.Location = new System.Drawing.Point(736, -3);
+            this.crow3.Name = "crow3";
+            this.crow3.Size = new System.Drawing.Size(100, 50);
+            this.crow3.TabIndex = 16;
+            this.crow3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 288);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "label2";
+            // 
+            // timerScore
+            // 
+            this.timerScore.Enabled = true;
+            this.timerScore.Tick += new System.EventHandler(this.timerScore_Tick);
+            // 
+            // crow1
+            // 
+            this.crow1.Image = ((System.Drawing.Image)(resources.GetObject("crow1.Image")));
+            this.crow1.Location = new System.Drawing.Point(70, 101);
+            this.crow1.Name = "crow1";
+            this.crow1.Size = new System.Drawing.Size(100, 35);
+            this.crow1.TabIndex = 21;
+            this.crow1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 326);
+            this.Controls.Add(this.crow1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.crow3);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.crow2);
+            this.Controls.Add(this.picDino);
+            this.Controls.Add(this.road);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picDino)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.road)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crow2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crow3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crow1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picDino;
+        private System.Windows.Forms.PictureBox road;
+        private System.Windows.Forms.PictureBox crow2;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox crow3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timerScore;
+        private System.Windows.Forms.PictureBox crow1;
+    }
+}
+
---
+++ AI-Bonus-Assignment-1/AI-Bonus-Assignment-1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AI_Bonus_Assignment_1
+{
+    public partial class Form1 : Form
+    {
+        int c = 0;
+        bool jump = false;
+        int speed = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerScore_Tick(object sender, EventArgs e)
+        {
+
+            crow3.Location = new Point(crow3.Location.X - speed, crow3.Location.Y);
+            crow1.Location = new Point(crow1.Location.X - speed, crow1.Location.Y);
+            crow2.Location = new Point(crow2.Location.X - speed, crow2.Location.Y);
+            road.Location = new Point(road.Location.X - speed, road.Location.Y);
+            pictureBox10.Location = new Point(pictureBox10.Location.X - speed, pictureBox10.Location.Y);
+            pictureBox11.Location = new Point(pictureBox11.Location.X - speed, pictureBox11.Location.Y);
+            pictureBox12.Location = new Point(pictureBox12.Location.X - speed, pictureBox12.Location.Y);
+            pictureBox6.Location = new Point(pictureBox6.Location.X - speed, pictureBox6.Location.Y);
+            pictureBox5.Location = new Point(pictureBox5.Location.X - speed, pictureBox5.Location.Y);
+            pictureBox9.Location = new Point(pictureBox9.Location.X - speed, pictureBox9.Location.Y);
+            pictureBox7.Location = new Point(pictureBox7.Location.X - speed, pictureBox7.Location.Y);
+            pictureBox8.Location = new Point(pictureBox8.Location.X - speed, pictureBox8.Location.Y);
+            pictureBox13.Location = new Point(pictureBox13.Location.X - speed, pictureBox13.Location.Y);
+            Invalidate();
+            if (picDino.Bounds.IntersectsWith(pictureBox10.Bounds))
+            {
+                jump = true;
+            }
+            else if (picDino.Bounds.IntersectsWith(pictureBox11.Bounds))
+            {
+                jump = true;
+            }
+            else if (picDino.Bounds.IntersectsWith(pictureBox12.Bounds))
+            {
+                jump = true;
+            }
+            ////////////////////////////////////////////////////////////////////
+            if (pictureBox10.Location.X < 11)
+            {
+                pictureBox10.Location = new Point(1000, pictureBox10.Location.Y);
+            }
+            if (pictureBox11.Location.X < 11)
+            {
+                pictureBox11.Location = new Point(1000, pictureBox11.Location.Y);
+            }
+            if (crow1.Location.X < 11)
+            {
+                crow1.Location = new Point(1000, crow1.Location.Y);
+            }
+            if (crow2.Location.X < 11)
+            {
+                crow2.Location = new Point(1000, crow2.Location.Y);
+            }
+            if (pictureBox12.Location.X < 11)
+            {
+                pictureBox12.Location = new Point(1000, pictureBox12.Location.Y);
+            }
+            if (crow3.Location.X < 11)
+            {
+                crow3.Location = new Point(1000, crow3.Location.Y);
+            }
+            if (pictureBox6.Location.X < 11)
+            {
+                pictureBox6.Location = new Point(1000, pictureBox6.Location.Y);
+            }
+            if (pictureBox5.Location.X < 11)
+            {
+                pictureBox5.Location = new Point(1000, pictureBox5.Location.Y);
+            }
+            if (pictureBox9.Location.X < 11)
+            {
+                pictureBox9.Location = new Point(1000, pictureBox9.Location.Y);
+            }
+            if (pictureBox7.Location.X < 11)
+            {
+                pictureBox7.Location = new Point(1000, pictureBox7.Location.Y);
+            }
+            if (pictureBox8.Location.X < 11)
+            {
+                pictureBox8.Location = new Point(1000, pictureBox8.Location.Y);
+            }
+            if (pictureBox13.Location.X < 11)
+            {
+                pictureBox13.Location = new Point(1000, pictureBox13.Location.Y);
+            }
+            if (jump)
+            {
+                if (c < 10)
+                {
+                    picDino.Location = new Point(picDino.Location.X, picDino.Location.Y - 9);
+                    c++;
+                }
+                else if (c > 9 && c < 20)
+                {
+                    picDino.Location = new Point(picDino.Location.X, picDino.Location.Y + 9);
+                    c++;
+                }
+                else
+                {
+                    jump = false;
+                    c = 0;
+                }
+            }
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
