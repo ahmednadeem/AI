a6f5633438976131e9212d9c2310aaf91dcc22a2
ahmednadeem <muhammadahmed644@gmail.com>
2018-04-11 23:24:13 +0500

    Add files via upload

---
+++ MapColoring(CSP)/MapColoring(CSP)/GraphColoringProblem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapColoring_CSP_
+{
+    public class GraphColoringProblem
+    {
+        int _v = 4;
+        int[] color;
+
+        public bool isSafe(int v, int[][] graph, int[] color, int c)
+        {
+            for (int i = 0; i < _v; i++)
+            {
+                if (graph[v][i] == 1 && c == color[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool graphColoringUtil(int[][] graph, int m,
+                              int[] color, int v)
+        {
+            if (v == _v)
+                return true;
+            for (int c = 1; c <= m; c++)
+            {
+                if (isSafe(v, graph, color, c))
+                {
+                    color[v] = c;
+                    if (graphColoringUtil(graph, m,
+                                          color, v + 1))
+                        return true;
+                    color[v] = 0;
+                }
+            }
+            return false;
+        }
+        public bool graphColoring(int[][] graph, int m)
+        {
+            
+            color = new int[_v];
+            for (int i = 0; i < _v; i++)
+                color[i] = 0;
+
+            
+            if (!graphColoringUtil(graph, m, color, 0))
+            {
+                Console.WriteLine("Solution does not exist");
+                return false;
+            }
+
+           
+            printSolution(color);
+            return true;
+        }
+        
+        void printSolution(int[] color)
+        {
+            Console.WriteLine("Solution Exists: Following" +
+                               " are the assigned colors");
+            for (int i = 0; i < _v; i++)
+                Console.Write(" " + color[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ MapColoring(CSP)/MapColoring(CSP)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapColoring_CSP_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphColoringProblem Coloring = new GraphColoringProblem();
+            // Create following graph and test whether it is colorable
+            //  (3)---(2)
+            //   |   / |
+            //   |  /  |
+            //   | /   |
+            //  (0)---(1)
+
+            int[][] graph =
+                {
+                new int[] { 0, 1, 1, 1 },
+                new int[] { 1, 0, 1, 1 },
+                new int[] { 1, 1, 0, 1 },
+                new int[] { 1, 0, 1, 0 },
+                };
+            int m = 3; // Number of colors
+            Coloring.graphColoring(graph, m);
+
+        }
+    }
+}
